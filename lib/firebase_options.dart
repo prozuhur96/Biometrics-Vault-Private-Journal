9c16dbe0f9c64f87cacb45179244411ad16a23bb
prozuhur96 <prozuhur96@gmail.com>
2026-09-21 23:03:59 +0200

    feat: add web options to firebase_option.dart and initialize app

---
+++ lib/firebase_options.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'PASTE_WEB_API_KEY_HERE',
+    appId: 'PASTE_WEB_APP_ID_HERE',
+    messagingSenderId: 'PASTE_SENDER_ID_HERE',
+    projectId: 'PASTE_PROJECT_ID_HERE',
+    authDomain: 'PASTE_PROJECT_ID_HERE.firebaseapp.com',
+    storageBucket: 'PASTE_PROJECT_ID_HERE.appspot.com',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'PASTE_ANDROID_API_KEY_HERE',
+    appId: 'PASTE_ANDROID_APP_ID_HERE',
+    messagingSenderId: 'PASTE_SENDER_ID_HERE',
+    projectId: 'PASTE_PROJECT_ID_HERE',
+    storageBucket: 'PASTE_PROJECT_ID_HERE.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'PASTE_IOS_API_KEY_HERE',
+    appId: 'PASTE_IOS_APP_ID_HERE',
+    messagingSenderId: 'PASTE_SENDER_ID_HERE',
+    projectId: 'PASTE_PROJECT_ID_HERE',
+    storageBucket: 'PASTE_PROJECT_ID_HERE.appspot.com',
+    iosBundleId: 'PASTE_YOUR_IOS_BUNDLE_ID_HERE',
+  );
+}
